e1dd97e2f8d5ac05c14bbdd271311cf531a43f64
zarif007 <zarifhuq786@gmail.com>
2023-02-19 23:25:13 +0600

    Adding terminal

--- portfolio/components/TerminalModal.tsx
+++ portfolio/components/TerminalModal.tsx
@@ -2,7 +2,16 @@ import React, { useEffect } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { Fragment, useState } from "react";
 import { AiOutlineCloseCircle } from "react-icons/ai";
-import ReactTextareaAutosize from "react-textarea-autosize";
+import { VscChromeMinimize } from "react-icons/vsc";
+import matrix from '../public/matrix.gif'
+import Image from "next/image";
+
+
+interface commandInterface {
+  location: string; 
+  command: string;
+  result: string
+}
 
 const TerminalModal = ({
   isTerminalOpen,
@@ -11,6 +20,27 @@ const TerminalModal = ({
   isTerminalOpen: boolean;
   setIsTerminalOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
+
+  const [commands, setCommands] = useState<commandInterface[]>([])
+  const [currentCommand, setCurrentCommand] = useState<string>('')
+
+  const [showMatrix, setShowMatrix] = useState<boolean>(false)
+
+  const handleAddCommand = (event: any) => {
+    
+    if (event.key === 'Enter') {
+      event.target.value = ''
+      setCommands([ ...commands, { location: 'user@portfolio-of-Zarif:~$', command: currentCommand, result: '' } ])
+      setCurrentCommand('');
+
+      setShowMatrix(true)
+
+      setInterval(() => setShowMatrix(false), 2000)
+    }
+
+  }
+
+
   return (
     <Transition appear show={isTerminalOpen} as={Fragment}>
       <Dialog
@@ -41,28 +71,40 @@ const TerminalModal = ({
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <Dialog.Panel className="w-full max-w-xl transform overflow-hidden bg-[#171717] border-2 rounded-md border-gray-200 text-left align-middle shadow-xl transition-all">
-                <div className="text-white p-2 font-semibold text-sm flex justify-between items-center">
-                    <p>POZ Terminal</p>
-                    <AiOutlineCloseCircle className="w-6 h-6 text-red-500" />
+              <Dialog.Panel className="w-full max-w-xl transform overflow-hidden bg-[#171717] border-2 rounded-md border-gray-700 text-left align-middle shadow-xl transition-all">
+                <div className="text-white p-2 font-semibold text-sm flex justify-between items-center bg-[#2E2E2E]">
+                  <p>POZ Terminal</p>
+                  <div className="flex space-x-2">
+                    <VscChromeMinimize className="w-6 h-6 text-blue-500 cursor-pointer" onClick={() => setIsTerminalOpen(false)} />
+                    <AiOutlineCloseCircle className="w-6 h-6 text-red-500 cursor-pointer" onClick={() => {
+                      setIsTerminalOpen(false)
+                      setCommands([])
+                      setCurrentCommand('')
+                    }} />
+                  </div>
                 </div>
 
-                <div className="border-t-2">
-                    <div className="p-2 text-md font-semibold">
-                        <p className="text-[#ff6932]">user@portfolio-of-Zarif:~$</p>
-                        <div className="flex items-start space-x-2">
-                            <p className="text-[#ff6932] mt-1">{'>'}</p>
-                            <form onSubmit={() => console.log('ooo')}>
-                                <ReactTextareaAutosize
-                                    className={`text-md bg-[#171717] text-gray-200 font-semibold mb-4 mt-1 px-1 focus:outline-0 rounded-md`}
-                                    minRows={2}
-                                    onChange={(e: any) => {
-                                    }}
-                                />
-                            </form>
-                        </div>
+                {
+                  showMatrix ? <Image src={matrix} alt="matrix" width={1000} height={1000} /> : <div className="border-t-2 border-gray-700  bg-[#171717]">
+                    <div className="p-2 text-md font-semibold pb-12">
+                      
+                      <ShowPreviousCommands commands={commands} />
+
+                      <p className="text-[#ff6932]">user@portfolio-of-Zarif:~$</p>
+                      <div className="flex items-start space-x-2">
+                        <p className="text-[#ff6932] mt-1">{">"}</p>
+                        <input
+                          className={`w-full pb-4 text-md bg-[#171717] text-gray-200 font-semibold mt-1 px-1 focus:outline-0 rounded-md`}
+                          defaultValue={currentCommand}
+                          onChange={(e: any) => setCurrentCommand(e.target.value)}
+                          onKeyDown={(event: React.KeyboardEvent<HTMLInputElement>) => handleAddCommand(event)}
+                        />
+                      </div>
+                      
                     </div>
-                </div>
+                  </div>
+                }
+
               </Dialog.Panel>
             </Transition.Child>
           </div>
@@ -71,5 +113,23 @@ const TerminalModal = ({
     </Transition>
   );
 };
+
+const ShowPreviousCommands = ({ commands }: { commands: commandInterface[]}) => {
+
+  return (
+    <>
+      {
+        commands.map((command: commandInterface) => {
+          return (
+            <div>
+              <p className="text-[#ff6932]">{command.location} <span className="text-gray-200">{command.command}</span></p>
+              <p className="text-gray-200">{command.result}</p>
+            </div>
+          )
+        })
+      }
+    </>
+  )
+}
 
 export default TerminalModal;
