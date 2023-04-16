13d63ad9efce86f11067fe6e531cb6b6fb1b9f83
zarif007 <zarifhuq786@gmail.com>
2023-04-16 23:02:14 +0600

    Adding code highlighter

--- portfolio/libs/codes.ts
+++ portfolio/libs/codes.ts
@@ -180,3 +180,32 @@ const Personality = () => {
 
 export default Personality;
 `
+
+export const skillsTechnology = `import React from 'react'
+import Head from 'next/head';
+import FootBar from '../../../components/FootBar';
+import Layout from '@/components/Layout';
+
+const Skills = () => {
+  return (
+    <div className='bg-[#2E2E2E]'>
+      <Head>
+        <title>Zarif</title>
+        <meta name="description" content="About Zarif" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
+      <Layout>
+      <div className=' text-gray-200 text-sm mt-12'>I’m a software engineer specializing in building (and occasionally designing) exceptional digital experiences. 
+              Currently, I’m focused on building accessible, human-centered products at</div>
+      </Layout>
+      
+
+      <FootBar />
+    </div>
+  )
+}
+
+export default Skills
+`
--- portfolio/pages/skills/technologies/index.tsx
+++ portfolio/pages/skills/technologies/index.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import Head from 'next/head';
 import FootBar from '../../../components/FootBar';
 import Layout from '@/components/Layout';
+import { skillsTechnology } from '@/libs/codes';
 
 const Skills = () => {
   return (
-    <div className='bg-[#2E2E2E]'>
+    <div className='bg-[#1e1e1e]'>
       <Head>
         <title>Zarif</title>
         <meta name="description" content="About Zarif" />
@@ -13,7 +14,7 @@ const Skills = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       
-      <Layout>
+      <Layout code={skillsTechnology}>
       <div className=' text-gray-200 text-sm mt-12'>I’m a software engineer specializing in building (and occasionally designing) exceptional digital experiences. 
               Currently, I’m focused on building accessible, human-centered products at</div>
       </Layout>
